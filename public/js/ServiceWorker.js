5d96e0e7a892a813559ebfeec15d3159e3125d17
Krunal Vijay Jayale <141128071+krunaljayale@users.noreply.github.com>
2024-05-29 23:10:12 +0530

    Update ServiceWorker.js

--- public/js/ServiceWorker.js
+++ public/js/ServiceWorker.js
@@ -14,7 +14,7 @@ const urlBase64ToUint8Array = base64String =>{
 }
 
 const saveSubscription = async (subscription) =>{
-    const response = await fetch('http://localhost:3000/service', {
+    const response = await fetch('https://magic-menu.onrender.com/service', {
         method:'post',
         headers:{'Content-type':"application/json"},
         body: JSON.stringify(subscription)
