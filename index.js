ae4b01ab0cfe6092dbd83fbef1890ab13f51b1ef
Krunal Jayale <krunaljayale@gmail.com>
2024-06-23 19:43:08 +0530

    Admin route crashed solved

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const User = require("./models/user.js");
 
 
 const dbUrl = process.env.ATLASDB_URL;
-const MONGO_URL = "mongodb://127.0.0.1:27017/cafe";
+
 
 async function main(){
     mongoose.connect(dbUrl);
